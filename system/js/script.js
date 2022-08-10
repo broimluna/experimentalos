6e60b53521a9cb497ff8950e777c297da94ba7c6
Luna <sashagirardhoule@gmail.com>
2022-08-10 11:36:25 -0400

    yes

---
+++ system/js/script.js
@@ -0,0 +1,281 @@
+var i = 0,
+minimizedWidth = new Array,
+minimizedHeight = new Array,
+windowTopPos = new Array,
+windowLeftPos = new Array,
+panel,
+id;
+
+
+
+setTimeout(function(){
+	document.getElementById("startupsnd").play(); 
+}, 10000)
+
+$(function() {
+	$(".openWindow").draggable();
+  });
+  
+
+function startTime(){
+    var date = new Date();
+    var h = date.getHours(); 
+    var m = date.getMinutes(); 
+	var d = date.getDate();
+	var mo = date.getMonth();
+	var y = date.getFullYear();
+	var t = document.getElementById("time");
+	var br = '<br>';
+   // var s = date.getSeconds(); 
+    var session = "AM";
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    //s = (s < 10) ? "0" + s : s;
+    
+	t.insertAdjacentHTML('beforeend', br);
+    var time = y + "-" + mo + "-" + d + " - " + h + ":" + m + " " + session;
+    document.getElementById("time").innerText = time;
+    document.getElementById("time").textContent = time;
+    setTimeout(startTime, 1000);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+function appstoggle() {
+	document.getElementById("appsmenu").classList.toggle("opened");
+}
+document.oncontextmenu = rightClick;
+  
+  function rightClick(clickEvent) {
+	  clickEvent.preventDefault();
+	  // return false;
+  }
+  document.onclick = hideMenu;
+  document.oncontextmenu = rightClick;
+	
+  function hideMenu() {
+	  document.getElementById("contextMenu")
+			  .style.display = "none"
+  }
+
+  function rightClick(e) {
+	  e.preventDefault();
+
+	  if (document.getElementById("contextMenu")
+			  .style.display == "block")
+		  hideMenu();
+	  else{
+		  var menu = document.getElementById("contextMenu")
+
+		  menu.style.display = 'block';
+		  menu.style.left = e.pageX + "px";
+		  menu.style.top = e.pageY + "px";
+	  }
+  }
+
+  function activatefull(ele) {
+	if (ele.requestFullscreen) {
+		ele.requestFullscreen();
+	}
+}
+
+// Function for full screen activation
+function deactivatefull() {
+	if (document.exitFullscreen) {
+		document.exitFullscreen();
+	}
+}
+
+function openTab(tabName) {
+var i;
+var x = document.getElementsByClassName("tab");
+for (i = 0; i < x.length; i++) {
+x[i].style.display = "none";
+}
+document.getElementById(tabName).style.display = "block";
+}
+
+$(document).ready(function(){
+	$("#appsmenu").click(function(){
+		document.getElementById("appsmenu").classList.toggle("opened");
+	  }, function(){
+		document.getElementById("appsmenu").classList.remove("opened");
+  
+	});
+  });			
+
+  var changeBG = function(event) {
+    var output = document.getElementById('desktopbg');
+    output.style.backgroundImage= "url("+URL.createObjectURL(event.target.files[0])+")";
+	
+  };
+
+  var resetBG = function(event) {
+	var output = document.getElementById('desktopbg');
+	output.style.backgroundImage= "";
+
+  }
+  
+  function enableWatermark() {
+	// Get the checkbox
+	var checkBox = document.getElementById("waterCheck");
+	// Get the output text
+	var text = document.getElementById("watermark");
+	// If the checkbox is checked, display the output text
+	if (checkBox.checked == true){
+	  text.style.display = "inline-block";
+	} else {
+	  text.style.display = "none";
+	}
+  }
+  function invertColors() {
+	// Get the checkbox
+	var checkBox = document.getElementById("colorCheck");
+	// Get the output text
+	var text = document.getElementById("main");
+	// If the checkbox is checked, display the output text
+	if (checkBox.checked == true){
+	  text.style.filter = "invert(1)";
+	} else {
+	  text.style.filter = "none";
+	}
+  }
+// Core Code
+
+function adjustFullScreenSize() {
+	$(".fullSizeWindow .wincontent").css("width", (window.innerWidth - 32));
+	$(".fullSizeWindow .wincontent").css("height", (window.innerHeight - 98));
+	setTimeout(adjustFullScreenSize, 10);
+}
+function makeWindowActive(thisid) {
+	$(".window").each(function() {      
+		$(this).css('z-index', $(this).css('z-index') - 1);
+	});
+	$("#window" + thisid).css('z-index',1000);
+	$(".window").removeClass("activeWindow");
+	$("#window" + thisid).addClass("activeWindow");
+	
+	$(".taskbarPanel").removeClass('activeTab');
+	
+	$("#minimPanel" + thisid).addClass("activeTab");
+}
+
+function minimizeWindow(id){
+	windowTopPos[id] = $("#window" + id).css("top");
+	windowLeftPos[id] = $("#window" + id).css("left");
+	
+	$("#window" + id).animate({
+		top: 800,
+		left: 0
+	}, 200, function() {		//animation complete
+		$("#window" + id).addClass('minimizedWindow');
+		$("#minimPanel" + id).addClass('minimizedTab');
+		$("#minimPanel" + id).removeClass('activeTab');
+	});	
+}
+
+function openWindow(id) {
+	if ($('#window' + id).hasClass("minimizedWindow")) {
+		openMinimized(id);
+	} else {	
+		makeWindowActive(id);
+		$("#window" + id).removeClass("closed");
+		$("#minimPanel" + id).removeClass("closed");
+	}
+}
+function closeWindwow(id) {
+	$("#window" + id).addClass("closed");
+	$("#minimPanel" + id).addClass("closed");
+}
+
+function openMinimized(id) {
+	$('#window' + id).removeClass("minimizedWindow");
+	$('#minimPanel' + id).removeClass("minimizedTab");
+	makeWindowActive(id);
+		
+	$('#window' + id).animate({
+		top: windowTopPos[id],
+		left: windowLeftPos[id]
+	}, 200, function() {
+	});				
+}
+
+$(document).ready(function(){
+	$(".window").each(function() {      		// window template
+		$(this).css('z-index',1000)
+		$(this).attr('data-id', i);
+		minimizedWidth[i] = $(this).width();
+		minimizedHeight[i] = $(this).height();
+		windowTopPos[i] = $(this).css("top");
+		windowLeftPos[i] = $(this).css("left");
+		$("#taskbar").append('<div class="taskbarPanel" id="minimPanel' + i + '" data-id="' + i + '">' + $(this).attr("icon") + '</div>');
+		if ($(this).hasClass("closed")) {	$("#minimPanel" + i).addClass('closed');	}		
+		$(this).attr('id', 'window' + (i++));
+		$(this).wrapInner('<div class="wincontent"></div>');
+		$(this).prepend('<div class="windowHeader"><strong>' + $(this).attr("data-title") + '</strong><span title="Minimize" class="winminimize"><span></span></span><span title="Maximize" class="winmaximize"><span></span><span></span></span><span title="Close" class="winclose">x</span></div>');
+	});
+	
+	$("#minimPanel" + (i-1)).addClass('activeTab');
+	$("#window" + (i-1)).addClass('activeWindow');
+	
+	$( ".wincontent" ).resizable();			// resizable
+	$( ".window" ).draggable({ cancel: ".wincontent" });	// draggable
+	
+
+    $(".window").mousedown(function(){		// active window on top (z-index 1000)
+		makeWindowActive($(this).attr("data-id"));
+    });
+	
+    $(".winclose").click(function(){		// close window
+		closeWindwow($(this).parent().parent().attr("data-id"));
+    });	
+
+    $(".winminimize").click(function(){		// minimize window
+		minimizeWindow($(this).parent().parent().attr("data-id"));
+    });	
+	
+    $(".taskbarPanel").click(function(){		// taskbar click
+		id = $(this).attr("data-id");
+		if ($(this).hasClass("activeTab")) {	// minimize if active
+			minimizeWindow($(this).attr("data-id"));
+		} else {
+			if ($(this).hasClass("minimizedTab")) {	// open if minimized
+				openMinimized(id);
+			} else {								// activate if inactive
+				makeWindowActive(id);
+			}
+		}
+    });	
+	
+    $(".openWindow").click(function(){		// open closed window
+		openWindow($(this).attr("data-id"));
+    });
+	
+    $(".winmaximize").click(function(){
+		if ($(this).parent().parent().hasClass('fullSizeWindow')) {			// minimize
+			
+			$(this).parent().parent().removeClass('fullSizeWindow');
+			$(this).parent().parent().children(".wincontent").height(minimizedHeight[$(this).parent().parent().attr("data-id")]);	
+			$(this).parent().parent().children(".wincontent").width(minimizedWidth[$(this).parent().parent().attr("data-id")]);
+		} else {															// maximize
+			$(this).parent().parent().addClass('fullSizeWindow');
+			
+			minimizedHeight[$(this).parent().parent().attr('data-id')] = $(this).parent().parent().children(".wincontent").height();
+			minimizedWidth[$(this).parent().parent().attr('data-id')] = $(this).parent().parent().children(".wincontent").width();
+			
+			adjustFullScreenSize();
+		}
+    });		
+	adjustFullScreenSize();	
+});
